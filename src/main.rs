5e9af89962ebee8064d631e65249f4b6ed91e33c
Benjamin H Boruff <11185240+benjaminboruff@users.noreply.github.com>
2023-01-09 15:32:38 -0500

    stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
 }
